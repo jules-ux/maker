857350b38219b3d23a66cde2e15909dcc91a3a7f
jules879 <115114666+jules879@users.noreply.github.com>
2025-01-07 21:30:12 +0100

    g

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
+import { createApp } from 'vue';          // Import Vue
+import { createVuetify } from 'vuetify';  // Import Vuetify
+import 'vuetify/styles'; 
 import App from './App.vue'
 
+const vuetify = createVuetify();         // Create Vuetify instance
+
+app.use(vuetify);                        // Use Vuetify with your app
 createApp(App).mount('#app')
