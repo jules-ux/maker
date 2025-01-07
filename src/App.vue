3ecbc032c13a17bf7eb3bc7265da60f95901b87d
jules879 <115114666+jules879@users.noreply.github.com>
2025-01-07 21:15:04 +0100

    y

--- src/App.vue
+++ src/App.vue
@@ -1,87 +1,16 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+ 
 </template>
 
 
 <script>
-import Top from './components/top-section.vue';
-import Tabel from './components/PrintTable.vue';
-import SearchBar from './components/search.vue';  // Voeg de zoekbalkcomponent toe
 
-export default {
-  name: 'ParentComponent',
-  components: {
-    Top,
-    Tabel,
-    SearchBar,  // Registreer de zoekbalkcomponent
-  },
-  data() {
-    return {
-      search: '',  // Zoekterm die we naar de tabel doorgeven
-    };
-  },
-  methods: {
-    updateSearchTerm(newTerm) {
-      this.search = newTerm;  // Werk de zoekterm bij wanneer de zoekbalk verandert
-    },
-  },
-};
 </script>
 
 <style>
-html::-webkit-scrollbar {
-  display: none; /* Hides the scrollbar */
-}
-.v-input__details {
-  display: none !important;
-}
-@media (max-width: 768px) {
-  .v-text-field>input {
-    text-align: center;
-  }
-}
-</style>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
