726859696f7646363ae1c2a2c271d43cf18e04aa
jules879 <115114666+jules879@users.noreply.github.com>
2025-01-07 21:00:17 +0100

    re

--- src/main.js
+++ src/main.js
@@ -1,7 +1,4 @@
-import { createApp } from 'vue';          // Import Vue
-import { createVuetify } from 'vuetify';  // Import Vuetify
 import { createApp } from 'vue'
-import 'vuetify/styles';                  // Import Vuetify styles
 import App from './App.vue'
 
 createApp(App).mount('#app')
